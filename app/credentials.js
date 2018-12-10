ecfa49c6208a256f494f8a48cbb1e21d3dd93233
liam missin <liam.missin@gmail.com>
2018-12-10 07:00:58 +0000

    use api server to get blocks

--- app/credentials.js
+++ app/credentials.js
@@ -4,10 +4,15 @@ module.exports = {
 	// You may delete this section if you wish to connect manually via the UI.
 
 	rpc: {
+		host:"35.178.184.167",
+		port:7990,
+		username:"CHANGEME",
+		password:"CHANGEME"
+	},
+
+	apiServer: {
 		host:"localhost",
-		port:8332,
-		username:"username",
-		password:"password"
+		port:8080
 	},
 
 	// optional: enter your api access key from ipstack.com below
