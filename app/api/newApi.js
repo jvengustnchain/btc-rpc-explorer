adb69e48cc7bdb92d7f9ca88a39fcf75cfa0103a
liam missin <liam.missin@gmail.com>
2018-12-24 23:38:25 +0000

    use post to get txids

--- app/api/newApi.js
+++ app/api/newApi.js
@@ -1,4 +1,5 @@
 const http = require('http')
+var rp = require('request-promise')
 var config = require('./../config.js')
 
 function getServiceURL () {
@@ -60,12 +61,13 @@ function getRawTransaction (txid) {
 
 function getRawTransactions (txids) {
   console.log('trying to get rawtransactions from new api!')
-  return getFromApiServer('gettransactions/' + txids)
+  // return getFromApiServer('gettransactions/' + txids)
+  return postToApiServer('gettransactions2', txids)
 }
 
 // getBlockByHashWithTransactions
 function getBlockTxIds (blockHash, offset, limit) {
-  console.log('trying to get rawtransaction from new api!')
+  console.log('trying to get blockTxids from new api!')
   return getFromApiServer('getblocktxids/' + blockHash + '/' + offset + '/' + limit)
 }
 
@@ -99,6 +101,55 @@ function getFromApiServer (path) {
     })
   })
 }
+
+function postToApiServer (path, jsonString) {
+  console.log('postToApiServer', getServiceURL() + path)
+  var options = {
+    method: 'POST',
+    uri: getServiceURL() + path,
+    body: {
+      txids: jsonString
+    },
+    json: true // Automatically stringifies the body to JSON
+  }
+
+  return rp(options)
+  // .then(function (parsedBody) {
+  //   console.log(parsedBody)
+  // })
+  // .catch(function (err) {
+  //   console.log(err)
+  // })
+}
+
+// return new Promise((resolve, reject) => {
+//   http.get(getServiceURL() + path, (resp) => {
+//     let data = ''
+//     // A chunk of data has been recieved.
+//     resp.on('data', (chunk) => {
+//       data += chunk
+//     })
+
+//     // The whole response has been received. Print out the result.
+//     resp.on('end', () => {
+//       if (data && data.length > 0) {
+//         try {
+//           var jsonResp = JSON.parse(data)
+//           resolve(jsonResp)
+//         } catch (error) {
+//           console.error('Error parsing data: ' + data)
+//           console.error('Error parsing data for path: ' + path)
+//           resolve(null)
+//         }
+//       } else {
+//         resolve(null)
+//       }
+//     })
+//   }).on('error', (err) => {
+//     console.log('Error: ' + err.message)
+//     reject(err)
+//   })
+// })
 
 module.exports = {
   getBlockByHash: getBlockByHash,
